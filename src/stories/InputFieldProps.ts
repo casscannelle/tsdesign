89157dad11429d10ef3ad95f492d35d7db8019e3
casscannelle <cassia.maffei@gmail.com>
2024-04-08 13:37:25 -0300

    organização em pastas

--- src/stories/InputFieldProps.ts
+++
@@ -1,8 +0,0 @@
-
-export interface InputFieldProps {
-    type?: string;
-    placeholder?: string;
-    value?: string;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    fontWeight?: 'regular' | 'bold';
-  }
